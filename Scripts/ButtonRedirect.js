6831a26a47b4552b7fbdba61153b8a11156c4ae2
Grizz <4hoursleft@gmail.com>
2022-09-08 21:25:02 +0800

    yes

--- Scripts/ButtonRedirect.js
+++ Scripts/ButtonRedirect.js
@@ -1,4 +1,3 @@
 function redirect() {
-    alert("well helllo")
-    //window.location.href = "https://www.youtube.com/watch?v=GtL1huin9EE"
+    window.location.href = "https://www.youtube.com/watch?v=GtL1huin9EE"
 }
