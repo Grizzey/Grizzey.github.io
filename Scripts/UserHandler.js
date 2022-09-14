45aabadb8578d05ca546432b816b1fec6f8095c6
Grizz <4hoursleft@gmail.com>
2022-09-14 20:21:17 +0800

    -stamp- (login works)

---
+++ Scripts/UserHandler.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    var userArray = [{
+            username: "admin",
+            password: "crazyrapidboots"
+        },
+        {
+            username: "user2",
+            password: "pw2"
+        },
+        {
+            username: "user3",
+            password: "pw3"
+        }
+    ]
+
+    function loginFunction() {
+        var username = document.getElementById("name").value;
+        var password = document.getElementById("password").value;
+
+        console.log(username, password)
+        for (i = 0; i < userArray.length; i++) {
+            if (username == userArray[i].username && password == userArray[i].password) {
+                alert("Hello");
+            } else {
+                console.warn("Skipped")
+            }
+        }
+    }
+
+    var loginsubmit = document.getElementById("login_button")
+    $(loginsubmit).click(function () {
+        loginFunction()
+    })
+
+})
